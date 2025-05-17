ed1ceb1fbbd682cc55a12538e407088e94e432cc
Alley-eddine <alleybenhassine@gamil.com>
2025-05-17 13:37:49 +0200

    init du back et du serveur express

---
+++ src/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const schema = require("./graphql/schema");
+
+const app = express();
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecté"))
+  .catch((err) => console.error(err));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${4000}`);
+});
