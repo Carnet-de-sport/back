d7f033e5391e7574bbfa57b5fde4c1b230f74b96
Alley-eddine <alleybenhassine@gamil.com>
2025-05-18 16:03:29 +0200

    Ajout de l'enum sur muscles et type pour Exercise côté Mongoose

--- src/models/Exercise.js
+++ src/models/Exercise.js
@@ -1,11 +1,34 @@
 const mongoose = require("mongoose");
 
+const MUSCLE_GROUPS = [
+  "Pectoraux",
+  "Dos",
+  "Épaules",
+  "Biceps",
+  "Triceps",
+  "Jambes",
+  "Fessiers",
+  "Abdominaux",
+  "Mollets",
+  "Avant-bras",
+  "Cou",
+];
+
+const EXERCISE_TYPES = [
+  "Force",
+  "Cardio",
+  "Souplesse",
+  "Équilibre",
+  "Hypertrophie",
+  "Puissance",
+];
+
 const ExerciseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   description: String,
-  muscles: [String],
-  type: String,
+  muscles: [{ type: String, enum: MUSCLE_GROUPS }], // <-- Menu déroulant/array limité aux valeurs
+  type: { type: String, enum: EXERCISE_TYPES },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
